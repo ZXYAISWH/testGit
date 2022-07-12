11303d4ecfbe31fba3501766acf7d7c507c8f27f
ZXYAISWH <3260653092@qq.com>
2022-07-12 19:17:23 +0800

    我的第三次测试

--- testGit-02/src/test/TestMain01.java
+++ testGit-02/src/test/TestMain01.java
@@ -3,5 +3,7 @@ package test;
 public class TestMain {
     public static void main(String[] args) {
         System.out.println("你好啊!!!");
+        System.out.println("你好啊!!!");
+        System.out.println("你好啊!!!");
     }
 }
